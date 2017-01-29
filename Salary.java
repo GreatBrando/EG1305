2380f42a298dc107efc3efb1c6f5141a27253923
GreatBrando <bboyfranco@att.net>
2017-01-29 15:00:14 -0600

    Salary

---
+++ Salary.java
@@ -0,0 +1,19 @@
+
+public class Salary 
+{
+	public static void main(String[] args)
+	{
+		//declare initialize
+		String name; // holds name
+		int age; // holds age
+		double annualPay; // holds annual wage
+		
+		name = "Brandon Franco";   
+		age = 19;  
+		annualPay = 150000.0;  
+		
+		System.out.println("My Name is " + name + ", my age is " + age + " and ");  
+		System.out.println("I hope to earn $" + annualPay + " per year.");  		//prints age, name , and desired annual income
+		
+	}
+}
